11a9320a46bfbca02e386e093fb543d9b959eba6
Mikee Rmz <mikee.rmz@gmail.com>
2019-04-02 17:23:29 -0600

    Add update from terminal
    Signed-off-by: Mikee Rmz <mikee.rmz@gmail.com>

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const { argv } =require('yargs')
+const { argv } = require('./config/yargs');
+const colors = require('colors');
+const toDo = require('./to-do/to-do');
 
-console.log(argv)
+const comando = argv._[0];
+
+switch (comando) {
+  case 'show':
+    const tasks = toDo.getList();
+    tasks.forEach(task => {
+      console.log("========To Do==========".green)
+      console.log('task: ', task.description)
+      console.log('status: ', task.complete)
+      console.log("=======================".green)
+    });
+    break;
+  case 'create':
+    toDo.create( argv.description ) 
+    break;
+  case 'update':
+    console.log('Update Tasks');
+    break;
+  default:
+    console.log("Comando no reconocido");
+    break;
+}
